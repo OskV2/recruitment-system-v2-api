350ba91f3c78d90004b766580750e8af3c8cf5f1
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2025-09-08 22:29:22 +0200

    feat: removed redundant code

--- src/modules/location/location.controller.ts
+++ src/modules/location/location.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import * as locationService from "./location.service";
 
 export const getLocation = async (req: Request, res: Response) => {
